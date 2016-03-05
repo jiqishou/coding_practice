51c35514c751cf3ccecf5880575421011d501e2d
Jason Chen <zhaochenmarch@gmail.com>
2016-03-04 16:20:27 -0800

    3/04

---
+++ factoriaN.cpp
@@ -0,0 +1,19 @@
+/*
+求n!
+*/
+
+int calculate(int n) {
+    if (n == 0 || n == 1) {
+        return 1;
+    }
+    
+    int prod = 1;
+    int prev = prod;
+    int i = 1;
+    while (i <= n) {
+        prod = i * prev;
+        prev = prod;
+        i++;
+    }
+    return prod;
+}
